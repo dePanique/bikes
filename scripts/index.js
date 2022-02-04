6cd06c925596e2458b72e577c84dd22f7b204f99
dePanique <de.simpl@yandex.ru>
2022-02-04 15:27:39 +0300

    feat: desktopversion

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,22 @@ picdown.addEventListener('pointerdown', (event) => {
     console.log(event)
   })
 })
+
+const bikes = document.querySelector('.bikes');
+
+let scrollWidth = Math.max(
+  document.body.scrollWidth, document.documentElement.scrollWidth,
+  document.body.offsetWidth, document.documentElement.offsetWidth,
+  document.body.clientWidth, document.documentElement.clientWidth
+);
+
+console.log(scrollWidth);
+
+window.onresize = function() {
+  scrollWidth = Math.max(
+    document.body.scrollWidth,
+    document.body.offsetWidth,
+    document.body.clientWidth,
+  );
+  console.log(scrollWidth);
+};
